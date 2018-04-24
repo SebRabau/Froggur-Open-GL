258d7cc908ec5ae1dbf40ad1a89d30ad46d5a4bb
Seb Rabau <strabau@gmail.com>
2018-04-24 14:10:10 +0100

    CMake run, added open-gl

---
+++ CMakeBuild/CMakeFiles/launch-playground-RelWithDebInfo.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/strab/Desktop/Froggur/Froggur-Open-GL/CI224-opengl-master/playground/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\strab\Desktop\Froggur\Froggur-Open-GL\CMakeBuild\RelWithDebInfo\playground.exe"  
+pause
