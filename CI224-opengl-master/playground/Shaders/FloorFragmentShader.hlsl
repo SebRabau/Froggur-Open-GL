57b3cebca262296640d60b924b556af1a6dac219
Seb Rabau <strabau@gmail.com>
2018-05-14 17:18:50 +0100

    shader and lighting updates

--- CI224-opengl-master/playground/Shaders/FloorFragmentShader.hlsl
+++ CI224-opengl-master/playground/Shaders/FloorFragmentShader.hlsl
@@ -10,7 +10,7 @@ uniform vec3 viewPos;
 
 void main()
 {
-	vec3 objectColor = vec3(1.0, 0.3, 0.6);
+	vec3 objectColor = vec3(0.8, 0.8, 0.8);
 	vec3 lightColor = vec3(1.0, 1.0, 1.0);
 
 	float ambientStrength = 0.1;
