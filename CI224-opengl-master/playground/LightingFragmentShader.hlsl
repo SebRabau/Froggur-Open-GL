cb1771c6838f818fb9b57fca0eaf576211e4044d
Seb Rabau <strabau@gmail.com>
2018-05-14 13:02:20 +0100

    Add files via upload

--- CI224-opengl-master/playground/LightingFragmentShader.hlsl
+++ CI224-opengl-master/playground/LightingFragmentShader.hlsl
@@ -6,10 +6,11 @@ in vec3 Normal;
 in vec3 FragPos;
 
 uniform vec3 lightPos;
+uniform vec3 viewPos;
 
 void main()
 {
-	vec3 objectColor = vec3(1.0, 1.0, 0.0);
+	vec3 objectColor = vec3(1.0, 0.3, 0.6);
 	vec3 lightColor = vec3(1.0, 1.0, 1.0);
 
 	float ambientStrength = 0.1;
@@ -21,6 +22,12 @@ void main()
 	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = diff * lightColor;
 
-	vec3 result = (ambient + diffuse) * objectColor;
+	float specularStrength = 0.5;
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 reflectDir = reflect(-lightDir, norm);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+	vec3 specular = specularStrength * spec * lightColor;
+
+	vec3 result = (ambient + diffuse + specular) * objectColor;
 	FragColor = vec4(result, 1.0);
 }
--- CI224-opengl-master/playground/LightingVertexShader.hlsl
+++ CI224-opengl-master/playground/LightingVertexShader.hlsl
@@ -1,18 +1,18 @@
 #version 330 core
+layout(location = 0) in vec3 aPos;
+layout(location = 2) in vec3 aNormal;
 
-layout(location = 0) in vec3 aPos; // the position variable has attribute position 0
-layout(location = 1) in vec3 aNormal; // the color variable has attribute position 1
+out vec3 FragPos;
+out vec3 Normal;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-out vec3 Normal;
-out vec3 FragPos;
-
 void main()
 {
-	gl_Position = projection * view * model * vec4(aPos, 1.0);
 	FragPos = vec3(model * vec4(aPos, 1.0));
-	Normal = aNormal;
+	Normal = mat3(transpose(inverse(model))) * aNormal;
+
+	gl_Position = projection * view * vec4(FragPos, 1.0);
 }
